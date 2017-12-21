1b146f7518f66fbe83b694cd217729b816e5bb5f
Keith Collier <keithcollier54@live.com>
2017-12-20 17:09:55 -0800

    Delete app.js

--- project5/js/app.js
+++
@@ -1,7 +0,0 @@
-function initMap() {
-        var reno = {lat: 39.5067, lng: -119.7899};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 4,
-          center: reno
-        });
-      }
