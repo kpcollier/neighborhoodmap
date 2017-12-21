9926688eb351d0b1b254f654ef0257ff7eb07e7c
Keith Collier <keithcollier54@live.com>
2017-12-21 10:09:59 -0800

    Add files via upload

---
+++ project5/js/app.js
@@ -0,0 +1,53 @@
+var map;
+var markers = [];
+function initMap() {
+        var reno = {lat: 39.5067, lng: -119.7899};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 12,
+          center: reno
+        });
+
+        var locations = [
+          {title: 'Eldorado Resort Casino', location: {lat: 39.52947, lng: -119.81497}},
+          {title: 'Atlantis Resort Casino', location: {lat: 39.48890, lng: -119.79369}},
+          {title: 'Silver Legacy Resort Casino', location: {lat: 39.53045, lng: -119.81517}},
+          {title: 'Circus Circus Reno', location: {lat: 39.53179, lng: -119.81529}},
+          {title: 'Grand Sierra Resort', location: {lat: 39.52316, lng: -119.77842}},
+          {title: 'Peppermill Resort Spa Casino', location: {lat: 39.49687, lng: -119.80213}}
+        ];
+
+        var infowindow = new google.maps.InfoWindow();
+
+        var bounds = new google.maps.LatLngBounds();
+
+        for (var i=0; i < locations.length; i++) {
+          var position = locations[i].location;
+          var title = locations[i].title;
+
+          var marker = new google.maps.Marker({
+            map: map,
+            position: position,
+            title: title,
+            animation: google.maps.Animation.DROP,
+            id: i
+          });
+
+          marker.addListener('click', function() {
+            if (infowindow.marker != this){
+              infowindow.setContent('');
+              infowindow.marker = this;
+            }
+            infowindow.addListener('closeclick', function() {
+              infowindow.marker = null;
+            });
+            infowindow.open(map, this);
+          });
+
+
+          markers.push(marker);
+          bounds.extend(marker.position);
+
+          map.fitBounds(bounds);
+
+        }
+};
