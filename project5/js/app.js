fc5e1e5540bbeb172a2fb383cfaeea4cbc5f9569
Keith Collier <keithcollier54@live.com>
2018-01-03 14:06:20 -0800

    Update app.js

--- project5/js/app.js
+++ project5/js/app.js
@@ -1,5 +1,15 @@
 var map;
 var markers = [];
+
+var locations = [
+  {title: 'Eldorado Resort Casino', location: {lat: 39.52947, lng: -119.81497}},
+  {title: 'Atlantis Casino Resort Spa', location: {lat: 39.48890, lng: -119.79369}},
+  {title: 'Silver Legacy Reno', location: {lat: 39.53045, lng: -119.81517}},
+  {title: 'Circus Circus Reno', location: {lat: 39.53179, lng: -119.81529}},
+  {title: 'Grand Sierra Resort', location: {lat: 39.52316, lng: -119.77842}},
+  {title: 'Peppermill Reno', location: {lat: 39.49687, lng: -119.80213}}
+];
+
 function initMap() {
         var reno = {lat: 39.5067, lng: -119.7899};
         var map = new google.maps.Map(document.getElementById('map'), {
@@ -7,21 +17,13 @@ function initMap() {
           center: reno
         });
 
-        var locations = [
-          {title: 'Eldorado Resort Casino', location: {lat: 39.52947, lng: -119.81497}},
-          {title: 'Atlantis Resort Casino', location: {lat: 39.48890, lng: -119.79369}},
-          {title: 'Silver Legacy Resort Casino', location: {lat: 39.53045, lng: -119.81517}},
-          {title: 'Circus Circus Reno', location: {lat: 39.53179, lng: -119.81529}},
-          {title: 'Grand Sierra Resort', location: {lat: 39.52316, lng: -119.77842}},
-          {title: 'Peppermill Resort Spa Casino', location: {lat: 39.49687, lng: -119.80213}}
-        ];
 
         var bounds = new google.maps.LatLngBounds();
 
         //Create Infowindow
         var largeInfowindow = new google.maps.InfoWindow();
 
-        var defaultIcon = makeMarkerIcon('0091ff');
+        var defaultIcon = makeMarkerIcon('00B4CC');
 
         var highlightedIcon = makeMarkerIcon('FFFF24');
 
@@ -45,7 +47,6 @@ function initMap() {
           bounds.extend(marker.position);
 
           marker.addListener('click', function() {
-            getData();
             populateInfoWindow(this, largeInfowindow);
           });
 
@@ -63,11 +64,35 @@ function initMap() {
         function populateInfoWindow(marker, infowindow) {
           if (infowindow.marker != marker) {
             infowindow.marker = marker;
-            infowindow.setContent(getContentString(marker));
-            infowindow.open(map, marker);
+            //infowindow.setContent(getContentString(marker));
+            //infowindow.open(map, marker);
             infowindow.addListener('closeclick', function() {
-              infowindow.setMarker(null);
+              infowindow.marker=null;
             });
+			var wikiUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + marker.title + '&format=json&callback=wikiCallback';
+			var articleStr;
+			var contentString = '<h3' + marker.title + '</h3';
+			$.ajax({
+				url: wikiUrl,
+				dataType: "jsonp",
+				beforeSend: function(){
+					infowindow.setContent('loading..');
+					infowindow.open(map,marker);
+				},
+				//jsonp : "callback"
+				success: function(response) {
+				var articleList = response[1];
+				console.log(response);
+				for (var i = 0; i < articleList.length; i++) {
+					articleStr = articleList[i];
+					var url = 'http://en.wikipedia.org/wiki/' + articleStr;
+					contentString = contentString + '<div class="infoWindow"><h1><strong>' + marker.title + '</strong></h1><br>' + '<p>' + "View Resort Article Here:" + '<p>' + '<a href=\"' + url + '\">' + url + '</a>' + '<br>';
+				};
+				//clearTimeout(wikiRequestTimeout);
+				infowindow.setContent(contentString);
+
+			  }
+			});
           }
         }
 
@@ -84,33 +109,20 @@ function initMap() {
 
 };
 
-function getData(){
-  var wikiUrl = 'http://wikipedia.org/w/api.php?action=&callback=wikiCallback';
-  var wikiRequestTimeout = setTimeout(function() {
-    alert("wiki failed")
-  }, 8000);
-  var articleStr;
-  var contentString = '<h3' + marker.title + '</h3';
-  $.ajax({
-    url: wikiUrl,
-    dataType: "jsonp",
-    //jsonp : "callback"
-    success: function(response) {
-      var articleList = response[1];
-      console.log(response);
-      for (var i = 0; i < articleList.length; i++) {
-        articleStr = articleList[i];
-        var url = 'http://en.wikipedia.org/wiki/' + articleStr;
-        contentString = contentString + '<a href=\"' + url + '\">' + url + '</a>' + '<br>';
+var myViewModel = function() {
+  var self = this;
 
-      };
-      clearTimeout(wikiRequestTimeout);
-    }
+  var places = function(data) {
+    this.title = ko.observable(data.title);
+    this.location = ko.observable(data.location);
+  }
+
+  this.placesList = ko.observableArray([]);
+
+  locations.forEach(function(placesItem) {
+    self.placesList.push(new places(placesItem));
   });
-}
 
-function getContentString(marker) {
-  var contentString = '<div class="infoWindow"><h3>' + marker.title + '</h3></div>';
+};
 
-  return contentString;
-}
+ko.applyBindings(new myViewModel());
